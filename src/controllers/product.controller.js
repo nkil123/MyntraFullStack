8d64bfc603076ba9866b9fbc6fe55b2b98631972
nkil123 <86908368+nkil123@users.noreply.github.com>
2021-12-14 16:19:46 +0530

    basic

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const Product = require ('../models/product.model');
+const router = express.Router ();
+
+router.post ('', async (req, res) => {
+  try {
+    const product = await Product.create (req.body);
+
+    return res.status (201).send ({product: product});
+  } catch (e) {
+    return res.status (500).json ({message: e.message, satus: 'Failed'});
+  }
+});
+router.get ('', async (req, res) => {
+  try {
+    const product = await Product.find ().lean ().exec ();
+
+    return res.status (201).send ({product: product});
+  } catch (e) {
+    return res.status (500).json ({message: e.message, satus: 'Failed'});
+  }
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ app.use (express.json ());
 
 app.set ('view engine', 'ejs');
 
+const mainProductController = require ('./controllers/mainProduct.controller');
+app.use ('/mainProducts', mainProductController);
+
+const productController = require ('./controllers/product.controller');
+app.use ('/products', productController);
+
 module.exports = app;
