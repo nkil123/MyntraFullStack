f9fb0a5b92ea1b1461d3cea0f9e4598757c23dfc
Dipankar Sarkar <sarkar.dipankar9999@gmail.com>
2021-12-16 00:41:42 +0530

    signup and login without navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const express = require ('express');
 
+
+
+const numberVerifyController =require("./controllers/signup-login/number-verify.controller");
+
+const {register,login} = require("./controllers/signup-login/user.controller");
+
 const app = express ();
 
 app.use (express.json ());
 
 app.set ('view engine', 'ejs');
 
-const mainProductController = require ('./controllers/mainProduct.controller');
-app.use ('/mainProducts', mainProductController);
+app.use("/signup-login",numberVerifyController);
 
-const productController = require ('./controllers/product.controller');
-app.use ('/products', productController);
+app.use("/signup",register);
+
+app.use("/login",login);
+
+
+
 
 module.exports = app;
---
+++ src/models/user.model.js
@@ -0,0 +1,43 @@
+const {Schema,model} = require("mongoose");
+
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new Schema({
+    name:{type:String, required:false},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    username:{type:String, required:true},
+    mobile:{type:Number, required:true},
+    description:{type:String, required:true}
+},{
+    versionKey:false,
+    timestamps:true
+});
+
+userSchema.pre("save", function (next){
+    if(! this.isModified("password"))
+    return next();
+
+    bcrypt.hash(this.password, 10, (err, hash)=> {
+        this.password = hash;
+        return next();
+    });
+
+});
+
+userSchema.methods.checkPassword = function (password){
+
+    return new Promise((resolve,reject)=>{
+        bcrypt.compare(password, this.password, function (err, res) {
+            if(err)  return reject(err);
+
+            return resolve(res);
+        
+        });
+
+    });
+
+}
+
+module.exports = model("users",userSchema);
