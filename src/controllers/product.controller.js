11025a10df0a5e33c1d854e9ac3beea36d7fdcd6
nkil123 <86908368+nkil123@users.noreply.github.com>
2021-12-18 11:42:43 +0530

    j

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,13 +14,39 @@ router.post ('', async (req, res) => {
 });
 router.get ('', async (req, res) => {
   try {
-    const products = await Product.find ().lean ().exec ();
+    console.log ('inside this');
+    const page = +req.query.page || 1;
+    const size = +req.query.limit || 20;
+    const offset = (page - 1) * size;
+    const products = await Product.find ()
+      .skip (offset)
+      .limit (size)
+      .lean ()
+      .exec ();
 
-    return res.status (201).send ({products: products});
+    return res.render ('productsPage.ejs', {products});
   } catch (e) {
     return res.status (500).json ({message: e.message, satus: 'Failed'});
   }
 });
+router.get ('/page', async (req, res) => {
+  try {
+    console.log ('inside this');
+    const page = +req.query.page || 1;
+    const size = +req.query.limit || 20;
+    const offset = (page - 1) * size;
+    const products = await Product.find ()
+      .skip (offset)
+      .limit (size)
+      .lean ()
+      .exec ();
+
+    return res.render ('productsPage.ejs', {products});
+  } catch (e) {
+    return res.status (500).json ({message: e.message, satus: 'Failed'});
+  }
+});
+
 router.get ('/:id', async (req, res) => {
   try {
     // console.log ('id find');
@@ -36,8 +62,9 @@ router.get ('/:id', async (req, res) => {
 
 router.get ('/category/:p1/:p2', async (req, res) => {
   try {
+    console.log ('inside category');
     // console.log (cat, req.query.parameter);
-    console.log (req.params.p1, req.params.p2);
+    // console.log (req.params.p1, req.params.p2);
     let products = await Product.find ().lean ().exec ();
     console.log (req.params.p1, req.params.p2);
     products = products.filter (p => {
@@ -52,7 +79,7 @@ router.get ('/category/:p1/:p2', async (req, res) => {
 });
 router.get ('/bags/:id', authenticate, async (req, res) => {
   try {
-    // console.log ('id find');
+    console.log ('inside bags');
     const product = await Product.findById (req.params.id).lean ().exec ();
     // console.log (product);
 
@@ -131,6 +158,7 @@ router.get ('/sort/:p', async (req, res) => {
     return res.status (500).json ({message: e.message, satus: 'Failed'});
   }
 });
+
 router.patch ('/:id', async (req, res) => {
   try {
     const product = await Product.findByIdAndUpdate (req.params.id, req.body, {
